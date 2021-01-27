d135e5c8caf7abd78acf9fb23c2ee746aa1a8b9c
datm20 <datm20@student.bth.se>
2021-01-27 14:14:58 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
-stuff
+
+#include <iostream>
+
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+std::cout << "Mängden argument" << argc << std::endl;
+for(int i = 0; i < argc; i++)
+{
+    std::cout << "Argumentet: " << argv[i] << std::endl;
+}
+
+
+
+
+return 0;
+}
